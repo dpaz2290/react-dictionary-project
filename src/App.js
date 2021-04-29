2a95ccae62d115ff3467408fdf1f3d846cdd2913
dpaz2290 <75946140+dpaz2290@users.noreply.github.com>
2021-04-29 12:17:53 -0500

    Search Engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-        Hello world
+        <div className= "container">
+          <main>
+            <Dictionary />
+          </main>
+          <footer className="text-center">  <small>Coded by Daniela Paz </small></footer>
+        </div>
     </div>
   );
 }
