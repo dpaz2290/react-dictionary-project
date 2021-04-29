19f35054dc9a018f1b33d4a68ae929d829cb15f3
dpaz2290 <75946140+dpaz2290@users.noreply.github.com>
2021-04-29 16:19:07 -0500

    Synonyms

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -14,13 +14,11 @@ export default function Dictionary (){
     }
 
     function handleResponse (response){
-        console.log(response.data[0]);
-        setResults(response.data[0])
+         setResults(response.data[0])
     }
 
     function search (event) {
         event.preventDefault();
-        alert(`Searching for ${keyword}`);
         let apiUrl =`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
     }
